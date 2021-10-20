bccdb213b22f424d08e741babfb55887f514d7db
Jason Li <jasonli1337@gmail.com>
2021-10-20 15:14:03 -0400

    Complete 1.2 Pipeline Assets

---
+++ Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+    //Protected-> only RenderPipelineAssets can override this
+    protected override RenderPipeline CreatePipeline()
+    {
+        return null;
+    }
+}
---
+++ Assets/Custom RP/Runtime/CustomeRenderPipeline.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomeRenderPipeline : RenderPipeline
+{
+
+}
