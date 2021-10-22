5c5a95b01e20437a3748fbcc2fdccfe60e22742f
Jason Li <jasonli1337@gmail.com>
2021-10-22 02:38:52 -0400

    3.6 Instanced Alpha Cutoffs
    Drawing Instances with Alpha Cutoff

--- Assets/Scripts/MeshBall.cs
+++ Assets/Scripts/MeshBall.cs
@@ -23,10 +23,13 @@ public class MeshBall : MonoBehaviour
         for (int i = 0; i < matrices.Length; i++)
         {
             matrices[i] = Matrix4x4.TRS(
-                Random.insideUnitSphere * 10f, Quaternion.identity, Vector3.one
+                Random.insideUnitSphere * 10f,
+                Quaternion.Euler(
+                    Random.value * 360f, Random.value * 360f, Random.value * 360f
+                ), Vector3.one * Random.Range(0.5f, 1.5f)
             );
             baseColors[i] =
-                new Vector4(Random.value, Random.value, Random.value, Random.value);
+                new Vector4(Random.value, Random.value, Random.value, Random.Range(0.5f, 1f));
         }
     }
 
