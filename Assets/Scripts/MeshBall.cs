6dbe2c927dc60df2f0ebba5f13d967663e9d0d23
Jason Li <jasonli1337@gmail.com>
2021-10-22 01:18:48 -0400

    2.4 DrawMeshInstanced Example
    1023 Meshes.
    
    MeshIndirect gets more through use of a buffer

---
+++ Assets/Scripts/MeshBall.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshBall : MonoBehaviour
+{
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
+
+    [SerializeField]
+    Mesh mesh = default;
+
+    [SerializeField]
+    Material material = default;
+
+    Matrix4x4[] matrices = new Matrix4x4[1023];
+    Vector4[] baseColors = new Vector4[1023];
+
+    MaterialPropertyBlock block;
+
+    //Create the positions and colors
+    void Awake()
+    {
+        for (int i = 0; i < matrices.Length; i++)
+        {
+            matrices[i] = Matrix4x4.TRS(
+                Random.insideUnitSphere * 10f, Quaternion.identity, Vector3.one
+            );
+            baseColors[i] =
+                new Vector4(Random.value, Random.value, Random.value, 1f);
+        }
+    }
+
+    void Update()
+    {
+        if (block == null)
+        {
+            block = new MaterialPropertyBlock();
+            block.SetVectorArray(baseColorId, baseColors);
+        }
+        //mesh, submesh index, material, matrices, elements, and property block
+        Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
+        //Note that your compute breaks this up into 511 instances a piece
+    }
+}
