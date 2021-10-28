16a9c287d7bdd8b58143b86ed35b0cb9a9c00505
Jason Li <jasonli1337@gmail.com>
2021-10-28 01:19:33 -0400

    2.2 Lighting Functions
    IncomingLight and GetLighting update

--- Assets/Custom RP/ShaderLibrary/Lighting.hlsl
+++ Assets/Custom RP/ShaderLibrary/Lighting.hlsl
@@ -1,9 +1,21 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
     #define CUSTOM_LIGHTING_INCLUDED
     
+    float3 IncomingLight(Surface surface, Light light)
+    {
+        return saturate(dot(surface.normal, light.direction)) * light.color;
+    }
+    
+    float3 GetLighting(Surface surface, Light light)
+    {
+        return IncomingLight(surface, light) * surface.color; //factor albedo into the lighting
+    }
+    
     float3 GetLighting(Surface surface)
     {
-        return surface.normal.y * surface.color; //factor albedo into the lighting
+        //Invoke GetDirectionalLight() from the Light.hlsl file
+        return GetLighting(surface, GetDirectionalLight());
     }
+    
     
 #endif
