f97d849b27971b5bd600d0871b2cf1fc1ed7c28d
Jason Li <jasonli1337@gmail.com>
2021-10-30 17:32:23 -0400

    5.2 Shader Property and Keyword Functions

--- Assets/Custom RP/Editor/CustomShaderGUI.cs
+++ Assets/Custom RP/Editor/CustomShaderGUI.cs
@@ -3,12 +3,89 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 
-public class CustomShaderGUI : CustomShaderGUI
+//A class to handle the custom GUI
+//We support multiple material rendering modes, and this will have one shader
+//But the multiple properties can be set as presets.
+
+public class CustomShaderGUI : ShaderGUI
 {
+    MaterialEditor editor;
+    Object[] materials; //because materialEditor.targets is Object[] so we match it here
+    MaterialProperty[] properties;
+
     public override void OnGUI(
-        MaterialEdtiro materialEdtor, MaterialProperty[] porperties
+        MaterialEditor materialEditor, MaterialProperty[] porperties
     )
     {
         base.OnGUI(materialEditor, porperties);
+        editor = materialEditor;
+        materials = materialEditor.targets;
+        this.properties = properties;
+    }
+
+    void SetProperty(string name, float value)
+    {
+        FindProperty(name, properties).floatValue = value;
+    }
+    void SetKeyword(string keyword, bool enabled)
+    {
+        if (enabled)
+        {
+            foreach (Material m in materials)
+            {
+                m.EnableKeyword(keyword);
+            }
+        }
+        else
+        {
+            foreach (Material m in materials)
+            {
+                m.DisableKeyword(keyword);
+            }
+        }
+    }
+
+    //A combined SetProperty for when we have a shader_feature keyword we can use
+    void SetProperty(string name, string keyword, bool value)
+    {
+        SetProperty(name, value ? 1f : 0f);
+        SetKeyword(keyword, value);
+    }
+
+    //A group of functions that set shader properties
+    bool Clipping
+    {
+        set => SetProperty("_Clipping", "_CLIPPING", value);
+    }
+
+    bool PremultiplyAlpha
+    {
+        set => SetProperty("_PremulAlpha", "_PREMULTIPLY_ALPHA", value);
+    }
+
+    BlendMode SrcBlend
+    {
+        set => SetProperty("_SrcBlend", (float)value);
+    }
+
+    BlendMode DstBlend
+    {
+        set => SetProperty("_DstBlend", (float)value);
+    }
+
+    bool ZWrite
+    {
+        set => SetProperty("_ZWrite", value ? 1f : 0f);
+    }
+
+    RenderQueue RenderQueue
+    {
+        set
+        {
+            foreach (Material m in materials)
+            {
+                m.renderQueue = (int)value;
+            }
+        }
     }
 }
--- Assets/Custom RP/Shaders/Lit.shader
+++ Assets/Custom RP/Shaders/Lit.shader
@@ -7,7 +7,7 @@ Shader "Custom RP/Lit"
         _BaseColor ("Color", Color) = (.5, .5, .5, 1.0)
         _Cutoff ("Alpha Cutoff", Range(0.0, 1.0)) = 0.5
         [Toggle(_CLIPPING)] _Clipping ("Alpha Clipping", Float) = 0
-        _Metallic ("Metalilic", Range(0, 1)) = 0
+        _Metallic ("Metallic", Range(0, 1)) = 0
         _Smoothness ("Smoothness", Range(0, 1)) = .5
         [Toggle(_PREMULTIPLY_ALPHA)] _PremulAlpha ("Premultiply Alpha", Float) = 0
         [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend ("Src Blend", Float) = 1 //self
