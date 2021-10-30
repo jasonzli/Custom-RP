e0dbca9ce18deae13b4f5a387dac9b7985045a90
Jason Li <jasonli1337@gmail.com>
2021-10-30 16:06:47 -0400

    3.4a Cleaned BRDF typo

--- Assets/Custom RP/ShaderLibrary/BRDF.hlsl
+++ Assets/Custom RP/ShaderLibrary/BRDF.hlsl
@@ -6,12 +6,13 @@
         float3 diffuse;
         float3 specular;
         float roughness;
-    }
+    };
     
     BRDF GetBRDF(Surface surface)
     {
         BRDF brdf;
-        bdrf.diffuse = surface.color;
+        float oneMinusReflectivity = 1.0 - surface.metallic; //metallics reflect via specular
+        brdf.diffuse = surface.color * oneMinusReflectivity;
         brdf.specular = 0.0;
         brdf.roughness = 1.0;
         return brdf;
